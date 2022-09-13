25f596b7b672c5ac8fa1bcdbcff5f2efaf4428b2
Khuram Naqvi <anaqvi527@gmail.com>
2022-09-13 15:58:27 +0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,4 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::post('/', [App\Http\Controllers\HomeController::class, 'chat']);
+Route::get('/', [App\Http\Controllers\HomeController::class, 'chat']);
