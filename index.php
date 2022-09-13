8391a27c2c23964eaa1d024d9ce8d05e5760cb43
Khuram Naqvi <anaqvi527@gmail.com>
2022-09-13 15:29:53 +0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+<span id="inputshow"></span><br/>
+<textarea > </textarea>
+
+<div contentEditable="true" class="textareass"><b>In computer science,</b> an x-fast trie is a data structure for storing integers from a bounded domain. It supports exact and predecessor or successor queries in time O(log log M), using O(n log M) space, where n is the number of stored values and M is the maximum value in the domain. The structure was proposed by Dan Willard in 1982,[1] along with the more complicated y-fast trie, as a way to improve the space usage of van Emde Boas trees, while retaining the O(log log M) query time.</div>
+</body>
+<iframe src="https://tokbox.com/embed/embed/ot-embed.js?embedId=b996be5a-c296-411e-a40e-6eb8e7b917b5&room=DEFAULT_ROOM&iframe=true" width="800px" height="640px" scrolling="auto" allow="microphone; camera" ></iframe>
+
+</html>
